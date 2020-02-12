df3bfad1b9881a94590b9931b477aa647f755bd0
Waridley <waridley64@gmail.com>
2020-02-11 19:16:09 -0600

    Add possible future alternative Credential backend

--- mongo/src/main/kotlin/com/waridley/textroid/mongo/credentials/MongoTextroidCredentialStorageBackend.kt
+++ mongo/src/main/kotlin/com/waridley/textroid/mongo/credentials/MongoTextroidCredentialStorageBackend.kt
@@ -1,4 +1,15 @@
 package com.waridley.textroid.mongo.credentials
 
-class MongoTextroidCredentialStorageBackend {
+import com.mongodb.client.MongoDatabase
+import com.waridley.textroid.credentials.TextroidCredential
+import com.waridley.textroid.mongo.getOrCreateCollection
+import org.litote.kmongo.eq
+import org.litote.kmongo.findOne
+
+class MongoTextroidCredentialStorageBackend(db: MongoDatabase, collectionName: String = "credentials") {
+	
+	val col = db.getOrCreateCollection<TextroidCredential>(collectionName)
+	
+	fun getCredentialByName(name: String) = col.findOne(TextroidCredential::credentialName eq name)
+	
 }
